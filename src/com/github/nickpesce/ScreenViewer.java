98e9c2ee36e79becb97bf10f92ad6152ab1b09ce
Nick Pesce <npesce@terpmail.umd.edu>
2016-09-01 22:32:58 -0400

    Updated to newest pixel transmission protocol and imporoved performance

--- src/com/github/nickpesce/ScreenViewer.java
+++ src/com/github/nickpesce/ScreenViewer.java
@@ -12,6 +12,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -20,6 +24,7 @@ import javax.swing.JFrame;
 
 public class ScreenViewer implements ActionListener, WindowListener{
 
+	public static final int NUM_LEDS = 60;
 	private Robot r;
 	private Sender sender;
 	private boolean running;
@@ -29,8 +34,11 @@ public class ScreenViewer implements ActionListener, WindowListener{
 	private JFrame frame;
 	private JButton bStart, bStop, bExit;
 	private Thread thread;
+	private String hostname;
+	private int port;
 	
-	public static void main(String[] args)
+	
+	public static void main(String[] args) throws IOException
 	{
 		ScreenViewer sv = new ScreenViewer();
 		sv.start();
@@ -39,11 +47,13 @@ public class ScreenViewer implements ActionListener, WindowListener{
 	/**
 	 * constructs and starts a new screenviewer. Continiously 
 	 * sends screen pixel data to a specified host.
+	 * @throws IOException 
 	 */
-	public ScreenViewer()
+	public ScreenViewer() throws IOException
 	{
 		running = true;
-		sender = new Sender("nickspi.student.umd.edu", 42297);
+		readConfig();
+		sender = new Sender(hostname, port);
 		screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
 		try {
 			r = new Robot();
@@ -91,32 +101,72 @@ public class ScreenViewer implements ActionListener, WindowListener{
 			@Override
 			public void run() {
 				stop();
+                sender.close();
 			}
 		}));
 	}
 	
 	/**
+	 * Reads hostname, port, and password from the config file. makes config if needed.
+	 * @throws IOException
+	 */
+	private void readConfig() throws IOException
+	{
+		File file = new File("config.txt");
+		if(!file.exists())
+		{
+			createConfig(file);
+		}
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line;
+		while((line = reader.readLine()) != null)
+		{
+			//Does not support ipv6 and some urls
+			String[] parts = line.split(" ");
+			if(parts[0].equals("hostname"))
+				hostname = parts[1].trim();
+			else if(parts[0].equals("port"))
+				port = Integer.parseInt(parts[1].trim());
+		}
+		reader.close();
+	}
+	
+	private void createConfig(File file) throws IOException 
+	{
+		file.createNewFile();
+		FileWriter writer = new FileWriter(file);
+		writer.write("hostname MY.HOST.NAME\n");
+		writer.write("port 12345\n");
+		writer.close();
+	}
+
+	/**
 	 * Get the screen pixel data, find averages, and send the data.
 	 */
 	public void update()
 	{
-		screen = r.createScreenCapture(screenRect);
-		int[][] leds = new int[60][3];
+		//screen = r.createScreenCapture(screenRect);
+		byte[] leds = new byte[NUM_LEDS * 3];
 		int[] pixel;
 		int redBucket = 0;
 		int greenBucket = 0;
 		int blueBucket = 0;
 		int pixelsInBucket = 0;
-		double colWidth = screen.getWidth()/60.0;
+		double colWidth = ((double)screenRect.getWidth())/NUM_LEDS;
 		for (int x = 0; x < 60; x++) {
-		    for (int y = 0; y < screen.getHeight(); y++) {
-		        pixel = screen.getRaster().getPixel((int)(x*colWidth), y, new int[3]);
+			screen = r.createScreenCapture(new Rectangle((int)(x*colWidth), 0, 1, (int)screenRect.getHeight()));
+		    for (int y = 0; y < screenRect.getHeight(); y++) {
+		        //pixel = screen.getRaster().getPixel((int)(x*colWidth), y, new int[3]);
+		        pixel = screen.getRaster().getPixel(0, y, new int[3]);
+
 		        redBucket += pixel[0];
 		        greenBucket += pixel[1];
 		        blueBucket += pixel[2];
 		        pixelsInBucket++;
 		    }
-		    leds[x] = new int[]{redBucket/pixelsInBucket, greenBucket/pixelsInBucket, blueBucket/pixelsInBucket};
+		    leds[3*x] = (byte)(redBucket/pixelsInBucket);
+		    leds[3*x + 1] = (byte)(greenBucket/pixelsInBucket);
+		    leds[3*x + 2] = (byte)(blueBucket/pixelsInBucket);
 		    pixelsInBucket = 0;
 		    redBucket = 0;
 		    blueBucket = 0;
@@ -135,14 +185,17 @@ public class ScreenViewer implements ActionListener, WindowListener{
 		thread = new Thread(new Runnable() {
 			@Override
 			public void run() {
+				long time = System.currentTimeMillis();
 				while(running)
 				{
 					update();
-					try {
-						Thread.sleep(50);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
+					System.out.println(System.currentTimeMillis() - time);
+					time = System.currentTimeMillis();
+//					try {
+//						Thread.sleep(50);
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
 				}
 			}
 		});
--- src/com/github/nickpesce/Sender.java
+++ src/com/github/nickpesce/Sender.java
@@ -1,70 +1,59 @@
 package com.github.nickpesce;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class Sender {
+	private String hostname;
+	private DatagramSocket socket;
 	private InetAddress host;
 	private int port;
-	private DatagramSocket socket;
 	
 	/**
 	 * Constructs a Sender object. It opens a socket and allows sending of pixel data.
 	 * @param hostName The host name to open the socket to
 	 * @param port The port to send to.
+	 * @throws UnknownHostException 
+	 * @throws SocketException 
 	 */
-	public Sender(String hostName, int port) {
+	public Sender(String hostName, int port) throws UnknownHostException, SocketException {
 		this.port = port;
-		 try
-         {
-             socket = new DatagramSocket();
-             host = InetAddress.getByName(hostName);
-         } catch (SocketException | UnknownHostException e)
-         {
-             e.printStackTrace();
-         }
+		this.hostname = hostName;
+		host = InetAddress.getByName(hostname);
+		socket = new DatagramSocket();
 	}
 	
 	/**
-	 * Sends a packet with the specified text to the host:port defined in the constructor.
-	 * Packet is a UDP datagram
-	 * @param text The message to send
+	 * Sends a packet with the specified byte array to the host:port defined in the constructor.
+	 * @param byte array in the form [r1, g1, b1, r2, g2, b2, ..., rn, gb, bn]
 	 */
-	private void sendPacket(final String text)
+	public void sendPixels(final byte[] leds)
     {
         new Thread(new Runnable(){
             public void run() {
-                try {
-                    byte[] buf = text.getBytes();
-                    DatagramPacket packet = new DatagramPacket(buf, buf.length, host, port);
-                    socket.send(packet);
-                }catch(Exception e)
-                {
-                    e.printStackTrace();
-                }
+                	DatagramPacket packet = new DatagramPacket(leds, leds.length, host, port);
+                    //Writes the led data to the socket output stream
+                	try {
+						socket.send(packet);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
             }
         }).start();
     }
-
-	/**
-	 * Converts and send an array of pixel arrays as a command string to the server.
-	 * @param leds The array of rgb arrays to send.
-	 */
-	public void sendPixels(int[][] leds) {
-		StringBuilder command = new StringBuilder("each -c ");
-		for(int[] led : leds)
-		{
-			command.append("(").append(led[0]).append(",").append(led[1]).append(",").append(led[2]).append(");");
-		}
-		command.deleteCharAt(command.length()-1);
-		sendPacket(command.toString());
-				
+	
+	public void sendOff()
+	{
+		sendPixels(new byte[ScreenViewer.NUM_LEDS]);
 	}
-
-	public void sendOff() {
-		sendPacket("stop");
+	
+	public void close() {
+        socket.close();
 	}
 }
